ff75e483c21eb5f0f90ccf7f427a52b35e3994b8
Dima Rekesh <bmwshop@gmail.com>
2024-04-20 18:21:40 -0700

    trying to expose InStringMetric

--- nemo/collections/common/metrics/__init__.py
+++ nemo/collections/common/metrics/__init__.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 
 from nemo.collections.common.metrics.classification_accuracy import TopKClassificationAccuracy
+from nemo.collections.common.metrics.classification_accuracy import InStringMatchMetric
 from nemo.collections.common.metrics.global_average_loss_metric import GlobalAverageLossMetric
 from nemo.collections.common.metrics.metric_string_to_torchmetric import MetricStringToTorchMetric
 from nemo.collections.common.metrics.perplexity import Perplexity
