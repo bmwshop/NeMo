43567f57f981d2da3250a63940ab3d17315f0a69
Dima Rekesh <bmwshop@gmail.com>
2024-04-20 17:44:59 -0700

    tracing

--- nemo/collections/common/metrics/classification_accuracy.py
+++ nemo/collections/common/metrics/classification_accuracy.py
@@ -214,6 +214,7 @@ class ExactStringMatchMetric(Metric):
 class InStringMatchMetric(Metric):
     def __init__(self, dist_sync_on_step=False, *args, **kwargs):
         super().__init__(dist_sync_on_step=dist_sync_on_step)
+        logging.info(f'InStringMatchMetric init')
 
         self.add_state("correct", default=torch.tensor(0), dist_reduce_fx="sum")
         self.add_state("total", default=torch.tensor(0), dist_reduce_fx="sum")
