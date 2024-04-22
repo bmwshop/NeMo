744c131c300e2996149d7aa0fae3e77696e4925e
Dima Rekesh <bmwshop@gmail.com>
2024-04-22 14:36:06 -0700

    typo

--- nemo/utils/callbacks/nemo_model_checkpoint.py
+++ nemo/utils/callbacks/nemo_model_checkpoint.py
@@ -180,7 +180,7 @@ class NeMoModelCheckpoint(ModelCheckpoint):
             if torch.distributed.is_initialized():
                 torch.distributed.barrier()
             # Dima: add step to filename
-            step_path = os.path.expanduser(os.path.join(self.dirpath, f'.{trainer.global_step}{self.postfix}')
+            step_path = os.path.expanduser(os.path.join(self.dirpath, f'.{trainer.global_step}{self.postfix}'))
             # pl_module.save_to(save_path=app_state.model_restore_path)
             pl_module.save_to(save_path=step_path)
             logging.info(f"New .nemo model saved to: {step_path}")
