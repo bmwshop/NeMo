8127069f7056fac4b207f8840b17f92ada8162a8
Dima Rekesh <bmwshop@gmail.com>
2024-04-23 09:54:30 -0700

    use time if trainer doesnt have steps

--- nemo/utils/callbacks/nemo_model_checkpoint.py
+++ nemo/utils/callbacks/nemo_model_checkpoint.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import time
 import os
 import re
 import shutil
@@ -180,7 +181,10 @@ class NeMoModelCheckpoint(ModelCheckpoint):
             if torch.distributed.is_initialized():
                 torch.distributed.barrier()
             # Dima: add step to filename
-            step_path = os.path.expanduser(os.path.join(self.dirpath, f'.{trainer.global_step}{self.postfix}'))
+            if trainer.global_step > 0:
+                step_path = os.path.expanduser(os.path.join(self.dirpath, f'{trainer.global_step}{self.postfix}'))
+            else: # if our trainer is weird, just use time
+                step_path = os.path.expanduser(os.path.join(self.dirpath, f'{time.time()}{self.postfix}'))
             # pl_module.save_to(save_path=app_state.model_restore_path)
             pl_module.save_to(save_path=step_path)
             logging.info(f"New .nemo model saved to: {step_path}")
